d6ac8a646d86417972270f89419cd285061747c5
Vincent Desrosiers <vincentdesrd@gmail.com>
2019-06-19 14:52:31 -0400

    Affichage post_title

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -1,4 +1,4 @@
-import {Component} from "react";
+import React, {Component} from "react";
 import axios from "axios";  
 
 export default class Tasks extends Component {
@@ -11,15 +11,20 @@ export default class Tasks extends Component {
   }
 
   fetchTasks() {
-    axios.get('http://localhost/TP2-Task-Manager/wp-json/taskmanager/v0/tasks').then(result => {
+    axios.get('http://localhost/TP2-Task-Manager/wp-json/taskmanagerx/v0/tasks/').then(result => {
       console.log(result);
       this.setState({ tasks: result.data });
     }).catch(error => {
       this.setState({ loading: false, error })
     })
+    
   }
-  render(){
-
-  }
-
+  render() 
+    {
+      return(
+      <ul>
+        {this.state.tasks.map(task => <li>task.post_title</li>)}
+      </ul>
+      )
+    }
 }
